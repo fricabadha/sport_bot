b975a4bd2f76c0795782d2cfbb952b21ad1a0732
Frica Badha <fexbadu@gmail.com>
2023-02-06 18:59:01 +0100

    feat: add goal and clear history

--- SportBot.py
+++ SportBot.py
@@ -1,6 +1,7 @@
 class SportBot:
     def __init__(self) -> None:
         self.data = []
+        self.goals = []
 
     def save_workout(self, user_id, time, distance, avg_speed):
         self.data.append({
@@ -15,12 +16,26 @@ class SportBot:
 
     def stats_workout():
         pass
-    
-    def save_goal():
-        pass
 
-    def delete_goal():
-        pass
+    def save_goal(self, user_id, distance):
+        self.goals.append({
+            'user_id': user_id,
+            'distance': distance
+        })
 
-    def clear_history():
-        pass
+    def return_goal(self, user_id):
+        for goal in self.goals:
+            if (user_id == goal['user_id']):
+                return goal
+        return None
+
+    def delete_goal(self, goal):
+        self.goals.remove(goal)
+
+    def clear_history(self, id):
+        new_data = []
+        workouts = self.history_workout()
+        for workout in workouts:
+            if id != workout['user_id']:
+                new_data.append(workout)
+        self.data = new_data
--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from dotenv import load_dotenv
 from telegram.ext import Updater, CommandHandler
-import os
 from SportBot import SportBot
+import os
+import logging
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',level=logging.INFO)
 
 load_dotenv()
 sportBot = SportBot()
@@ -20,7 +23,12 @@ def save(update, context):
     id = update.effective_chat.id
     time, distance, avg_speed = context.args
     sportBot.save_workout(id, time, distance, avg_speed)
-    update.message.reply_text("Dati appena salvati")
+    goal = sportBot.return_goal(id)
+    if(goal):
+        if float(distance) >= float(goal['distance']):
+            update.message.reply_text("Hai raggiunto il tuo obiettivo, impostane uno nuovo!")
+            sportBot.delete_goal(goal)
+    update.message.reply_text("Dati salvati")
 
 
 def history(update, context):
@@ -31,6 +39,21 @@ def history(update, context):
             update.message.reply_text("Tempo: %s, Distanza: %s, Velocità media: %s" % (
                 workout['time'], workout['distance'], workout['avg_speed']))
 
+def goal(update, context):
+    id = update.effective_chat.id
+    distance = context.args[0]
+    goal = sportBot.return_goal(id)
+    if(goal):
+        sportBot.delete_goal(goal)
+    sportBot.save_goal(id, distance)
+    update.message.reply_text("Obiettivo di distanza memorizzato.")
+
+
+def clear(update, context):
+    id = update.effective_chat.id
+    sportBot.clear_history(id)
+    update.message.reply_text("Dati eliminati")
+    
 
 def error(update, context):
     print('Update "%s" caused error "%s"' % (update, context.error))
@@ -45,8 +68,10 @@ if __name__ == "__main__":
     dp.add_handler(CommandHandler("help", help))
     dp.add_handler(CommandHandler("save", save))
     dp.add_handler(CommandHandler("history", history))
+    dp.add_handler(CommandHandler('goal', goal))
+    dp.add_handler(CommandHandler('clear', clear))
 
-    dp.add_error_handler(error)
+    # dp.add_error_handler(error)
 
     updater.start_polling()
     updater.idle()
