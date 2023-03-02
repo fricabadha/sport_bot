238c991ca39cdf5111541baf0ea7021474dcc16e
Frica Badha <fexbadu@gmail.com>
2023-03-02 23:37:34 +0100

    feat: add data storage, add stats command, clear history

--- SportBot.py
+++ SportBot.py
@@ -1,41 +1,107 @@
+import mysql.connector
+import math
+
+mydb = mysql.connector.connect(
+    host="127.0.0.1",
+    user="root",
+    password="sesso6969",
+    database="sportBot"
+)
+
+conn = mydb.cursor()
+conn.execute("CREATE TABLE IF NOT EXISTS workouts (id BIGINT AUTO_INCREMENT PRIMARY KEY, user_id BIGINT, time TEXT, distance FLOAT)")
+conn.execute(
+    "CREATE TABLE IF NOT EXISTS goals (id BIGINT AUTO_INCREMENT PRIMARY KEY,user_id BIGINT, distance FLOAT)")
+
+
 class SportBot:
     def __init__(self) -> None:
-        self.data = []
-        self.goals = []
-
-    def save_workout(self, user_id, time, distance, avg_speed):
-        self.data.append({
-            'user_id': user_id,
-            'time': time,
-            'distance': distance,
-            'avg_speed': avg_speed
-        })
-
-    def history_workout(self):
-        return self.data
-
-    def stats_workout():
         pass
 
+    def save_workout(self, user_id, time, distance):
+        conn.execute(
+            "INSERT into workouts (user_id, time, distance) values (%s, %s, %s)", (user_id, time, distance))
+        mydb.commit()
+
+    def history_workout(self, user_id):
+        conn.execute("SELECT * FROM workouts where user_id = %s", (user_id,))
+        workouts = conn.fetchall()
+        return workouts
+
+    def stats_workout(self, user_id):
+        conn.execute("SELECT * FROM workouts where user_id = %s", (user_id,))
+        workouts = conn.fetchall()
+        if len(workouts) == 0:
+            return
+        sum_ore = 0
+        sum_km = 0
+        for workout in workouts:
+            print("Tempo: ", workout[2], self.convert_time_in_sec(workout[2]))
+            print("Dustanza: ", workout[3])
+            sum_ore+=self.convert_time_in_sec(workout[2])
+            sum_km+=workout[3]
+        print("Totale ore in sec: ", sum_ore)
+        print("Totale ore: ", self.convert_sec_in_time(sum_ore))
+        media = sum_ore/max(len(workouts), 1)
+        ogg = {
+            "tot_ore" : self.convert_sec_in_time(sum_ore),
+            "media_ore" : self.convert_sec_in_time(media),
+            "tot_km" : sum_km,
+            "media_km" : round(sum_km/max(len(workouts), 1), 2),
+            "avg_speed" : round(self.avg_speed(self.convert_sec_in_time(sum_ore), sum_km), 2),
+            "tot_works" : len(workouts)
+        }
+        return ogg
+
     def save_goal(self, user_id, distance):
-        self.goals.append({
-            'user_id': user_id,
-            'distance': distance
-        })
+        res = self.return_goal(user_id)
+        if res:
+            conn.execute("DELETE FROM goals where user_id = %s", (user_id,))
+        conn.execute(
+            "INSERT into goals (user_id, distance) values (%s, %s)", (user_id, distance))
+        mydb.commit()
 
     def return_goal(self, user_id):
-        for goal in self.goals:
-            if (user_id == goal['user_id']):
-                return goal
+        conn.execute("SELECT * FROM goals where user_id = %s", (user_id,))
+        goals = conn.fetchall()
+        for goal in goals:
+            return goal
         return None
 
-    def delete_goal(self, goal):
-        self.goals.remove(goal)
+    def delete_goal(self, user_id):
+        conn.execute("DELETE FROM goals where user_id = %s", (user_id,))
+        mydb.commit()
 
-    def clear_history(self, id):
-        new_data = []
-        workouts = self.history_workout()
-        for workout in workouts:
-            if id != workout['user_id']:
-                new_data.append(workout)
-        self.data = new_data
+    def clear_history(self, user_id):
+        conn.execute("DELETE FROM workouts where user_id = %s", (user_id,))
+        mydb.commit()
+
+    def avg_speed(self, time, distance):
+        splitted_time = time.split(":")
+        sec = int(splitted_time[2])
+        min = int(splitted_time[1])
+        ore = int(splitted_time[0])
+
+        ore += min/60
+        ore += sec/3600
+
+        return round(distance/ore, 3)
+    
+    def convert_time_in_sec(self, time):
+        splitted_time = time.split(":")
+        sec = int(splitted_time[2])
+        min = int(splitted_time[1])
+        ore = int(splitted_time[0])
+        return (ore*3600)+(min*60)+sec
+    
+    def convert_sec_in_time(self, sec):
+        ore = math.floor(sec/3600)
+        min = math.floor((sec%3600)/60)
+        sec = math.floor((sec%3600)%60)
+
+        l = [ore, min, sec]
+
+        time = ':'.join(str(n) for n in l)
+        return time
+
+
--- main.py
+++ main.py
@@ -3,51 +3,68 @@ from telegram.ext import Updater, CommandHandler
 from SportBot import SportBot
 import os
 import logging
+import mysql.connector
+import re
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',level=logging.INFO)
 
 load_dotenv()
 sportBot = SportBot()
 
-
 def start(update, context):
     update.message.reply_text(
         "Ciao! Sono il tuo assistente per la tracciatura degli allenamenti di corsa. Invia il comando /help per avere la lista dei comandi disponibili.")
 
-
 def help(update, context):
-    update.message.reply_text("Questo è l'aiuto del bot")
-
+    update.message.reply_text("Lista dei comandi disponibili:\n\n"
+                              "/start - Avvia il bot\n"
+                              "/help - Mostra questo messaggio di aiuto\n"
+                              "/save - Salva i dati del proprio allenamento\n"
+                              "/history - Mostra la cronologia di tutti gli allenamenti\n"
+                              "/goal - Imposta un obiettivo di distanza\n"
+                              "/stats - Mostra statistiche complessive: Totale e media di km percorsi, tempo impiegato, velocità media\n"
+                              "/clear - Elimina tutti i dati.")
 
 def save(update, context):
     id = update.effective_chat.id
-    time, distance, avg_speed = context.args
-    sportBot.save_workout(id, time, distance, avg_speed)
+    time, distance = context.args
+    rx = r"^([01]?\d|2[0-3]):([0-5]\d):([0-5]\d)$"
+    matches = re.findall(rx, time)
+    if len(matches) == 0:
+        update.message.reply_text("Formato del tempo non valido, deve essere HH:MM:SS")
+        return
+    sportBot.save_workout(id, time, distance)
+    
     goal = sportBot.return_goal(id)
+    print(goal)
     if(goal):
-        if float(distance) >= float(goal['distance']):
+        if float(distance) >= float(goal[2]):
             update.message.reply_text("Hai raggiunto il tuo obiettivo, impostane uno nuovo!")
-            sportBot.delete_goal(goal)
+            sportBot.delete_goal(id)
     update.message.reply_text("Dati salvati")
 
 
 def history(update, context):
     id = update.effective_chat.id
-    workouts = sportBot.history_workout()
+    workouts = sportBot.history_workout(id)
     for workout in workouts:
-        if id == workout['user_id']:
-            update.message.reply_text("Tempo: %s, Distanza: %s, Velocità media: %s" % (
-                workout['time'], workout['distance'], workout['avg_speed']))
+        update.message.reply_text("Time: %s, Distance: %s, Average Speed: %s" % (workout[2], workout[3], sportBot.avg_speed(workout[2], workout[3])))
 
+    
 def goal(update, context):
     id = update.effective_chat.id
     distance = context.args[0]
-    goal = sportBot.return_goal(id)
-    if(goal):
-        sportBot.delete_goal(goal)
     sportBot.save_goal(id, distance)
     update.message.reply_text("Obiettivo di distanza memorizzato.")
 
+def stats(update, context):
+    id = update.effective_chat.id
+    res = sportBot.stats_workout(id)
+    print(res)
+    if res == None:
+        update.message.reply_text("Non ci sono allenamenti")
+    else:
+        update.message.reply_text("Totale ore: %s,\nMedia ore: %s,\nTotale km: %s,\nMedia km: %s,\nVelocità media: %s,\nTotale allenamenti: %s" % (res["tot_ore"], res["media_ore"], res["tot_km"], res["media_km"],res["avg_speed"], res["tot_works"]))
 
 def clear(update, context):
     id = update.effective_chat.id
@@ -69,6 +86,7 @@ if __name__ == "__main__":
     dp.add_handler(CommandHandler("save", save))
     dp.add_handler(CommandHandler("history", history))
     dp.add_handler(CommandHandler('goal', goal))
+    dp.add_handler(CommandHandler('stats', stats))
     dp.add_handler(CommandHandler('clear', clear))
 
     # dp.add_error_handler(error)
